4e87373699bdde73a1deaa42ee2521702a67e2c2
mithilesh <mithilesht@cloudthat.com>
2024-02-08 16:55:09 +0530

    updated whitelisted domains

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var corOptions = {
   origin: [
     'https://todolist-rust-psi.vercel.app',
     'http://localhost:5173',
+    'http://localhost:5174',
     'http://localhost:8080',
     'http://localhost:3000',
     'https://todo-sooty-nu.vercel.app',
